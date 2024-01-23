2e6b6828fcc439ac3a8bebed62362a2633abc566
LoesBijman <loesbijman@gmail.com>
2024-01-23 19:01:11 +0100

    move_particles_diffuse improved
    move_particles_diffuse function now works. One last issue with it:
    - it seems that the array wraps around both horizontally and vertically right now, meaning that nutrients can go from left to right but also from top to bottom (not very realistic)
    
    The init-array function is meant to generate an array where the nutrients are placed randomly on a grid. Right now the function does not place any nutrients on the top row, which is quite weird. Esther mentioned we should look at this after discussing how we want to initialize the 'stream of nutrients'. Let's look at that one tomorrow.

--- Project/Code/dla_model.py
+++ Project/Code/dla_model.py
@@ -11,42 +11,47 @@ def init_particles(size, prop_particles):
     n_elements = size ** 2
     n_particles = int(prop_particles * n_elements)
 
-    init = np.zeros((size,size), dtype = int)
+    init_array = np.zeros((size,size), dtype = int)
 
+    # initialize the particles randomly
     particles_indices = np.random.choice(n_elements, n_particles, replace = False)
-    init.flat[particles_indices] = 1
+    init_array.flat[particles_indices] = 1
 
-    return init
+    return init_array
 
-def move_particles_diffuse(init):
+def move_particles_diffuse(init_array):
     '''
-    Changes the particles in init in a random way. 
+    Changes the particles in init_array in a random way. 
     They can move up, down, to the left or to the right.
     Returns the array after one step.
     '''
     moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    particle_coordinates = np.column_stack(np.where(init == 1))
+    particle_coordinates = np.column_stack(np.where(init_array == 1))
+    bound = init_array.shape[0]
 
     for particle in particle_coordinates:
-            x, y = particle
+        x, y = particle
+        particle_index = x * bound + y
 
-            # randomly choose a step
-            dx, dy = moves[np.random.choice(len(moves))]
-            bound = init.shape[0]
+        # randomly choose a step
+        dx, dy = moves[np.random.choice(len(moves))]
 
-            # update particle position
-            new_x, new_y = (x + dx) % bound , (y + dy) % bound # figure wraps around itself
+        # update particle position
+        new_x, new_y = x + dx, (y + dy) % bound  # wrap around horizontally
 
-            # if the particle bumps into another particle, it remains where it is
-            if new_x in particle_coordinates[:,0] or new_y in particle_coordinates[:,1]:
-                 new_x, new_y = x, y
-                 
+        if new_x > bound:
+            new_x = x
+
+        # if the particle bumps into another particle, it remains where it is
+        if (new_x, new_y) not in particle_coordinates.tolist():
             # update the particle's position and init array
             new_particle_index = new_x * bound + new_y
+            updated_array = init_array.copy()
+            updated_array.flat[particle_index], updated_array.flat[new_particle_index] = 0, 1
+        else:
+            updated_array = init_array
 
-            init.flat[particle], init.flat[new_particle_index] = 0, 1
-
-    return init
+    return updated_array
 
 def move_particles_laminar():
     pass
