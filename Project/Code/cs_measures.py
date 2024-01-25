4df5e6c07be7ab6bb8859f02453782770619d33c
Boyan Mihaylov <mih.boyan@gmail.com>
2024-01-25 13:08:38 +0100

    Test plots

--- Project/Code/cs_measures.py
+++ Project/Code/cs_measures.py
@@ -17,41 +17,45 @@ def fractal_dimension(lattice_array):
 
     box_exponent = int(box_exponent)
     dim_box_series = np.empty(box_exponent)
+    n_box_series = np.empty(box_exponent)
+    scale_inverse_series = np.empty(box_exponent)
 
     # Iterate over different lattice resolutions
     for k in range(1, box_exponent + 1):
-        box_size = lattice_size // k
-
-        print(box_size)
-
-        # Count number of boxes including occupied lattice sites
+        n_boxes = 2 ** k
+        box_size = lattice_size // n_boxes
 
         # Create a series of intervals
-        intervals = [((i * box_size, (i + 1) * box_size - 1) for i in range(k))]
-        intervals_product = list(product(intervals, repeat=lattice_dims))
+        partitions = np.arange(n_boxes + 1) * box_size
+        slices_1D = [slice(partitions[i], partitions[i + 1]) for i in range(n_boxes)]
 
         # Create n-dimensional slices
-        slices = [slice(ip[0], ip[1]) for ip in intervals_product]
-        lattice_regions = lattice_array[tuple(slices)]
+        slices = tuple(product(slices_1D, repeat=lattice_dims))
 
-        print(lattice_regions.shape)
-        # region_sums = np.sum(lattice_regions)
-        # n_box_occupied = asd
-
-        # n_box_occupied = 0
-        # for i in range(k):
-
-        #     # Create a list of slice objects
-        #     slices = [slice(i * box_size, (i + 1) * box_size - 1) for _ in range(lattice_dims)]
-
-        #     # Extract square region
-        #     lattice_region = lattice_array[tuple(slices)]
-        #     if np.sum(lattice_region) > 0:
-        #         n_box_occupied += 1
+        # Count number of boxes including occupied lattice sites
+        n_box_occupied = 0
+        for s in slices:
+            # Extract square region
+            lattice_region = lattice_array[s]
+            if np.sum(lattice_region) > 0:
+                n_box_occupied += 1
         
         # # Calculate Minkowski dimension estimate
-        # dim_box = math.log(n_box_occupied) / math.log(box_size)
+        dim_box = math.log(n_box_occupied) / math.log(lattice_size/box_size)
 
-        # dim_box_series[k - 1] = dim_box
+        dim_box_series[k - 1] = dim_box
+        n_box_series[k - 1] = n_box_occupied
+        scale_inverse_series[k - 1] = lattice_size/box_size
     
-    # return dim_box_series
+
+    # Perform least-squares regression on results
+    log_n_box_series = np.log(n_box_series)
+    log_scale_inverse_series = np.log(scale_inverse_series)
+    # print(log_n_box_series[:, np.newaxis])
+    # reg_solution = np.linalg.lstsq(log_n_box_series[:, np.newaxis], log_scale_inverse_series)
+    # print(reg_solution)
+
+    coeffs = np.polyfit(log_scale_inverse_series, log_n_box_series, 1)
+    print(coeffs)
+
+    return dim_box_series, n_box_series, scale_inverse_series, coeffs
--- Project/Code/vis_tools.py
+++ Project/Code/vis_tools.py
@@ -28,3 +28,24 @@ def animate_lattice_2D(lattice_data_frames, interval=100):
     anim = animation.FuncAnimation(fig, animate, frames=n_frames, interval=interval, blit=True)
 
     return HTML(anim.to_html5_video())
+
+
+def plot_fractal_dimension(n_box_series, scale_inverse_series, coeffs):
+    """
+    Plots the relationship between N(epsilon) (number of boxes of size epsilon)
+    and 1/epsilon (inverse of box size) on a log-log plot, illustrating the analysed fractal dimension
+    """
+
+    fig, ax = plt.subplots()
+
+    ax.loglog(n_box_series, scale_inverse_series, marker='o')
+    coeffs = np.exp(coeffs)
+    plot_min = np.min(n_box_series)
+    plot_max = np.max(n_box_series)
+    ax.plot([plot_min, plot_max], [plot_min * coeffs[0] + coeffs[1], plot_max * coeffs[0] + coeffs[1]])
+
+    ax.set_xlabel("$1/\epsilon$")
+    ax.set_ylabel("$N(\epsilon)$")
+    fig.suptitle("Scaling factor of lattice vs number of occupied lattice sites")
+
+    plt.show()
